674121242a608c438fc8ed7b231434fa7589b314
Bill Gathen <bill@billgathen.com>
2015-10-30 17:37:04 -0400

    Switch page rendering to rely on keys pressed instead of notes sounding, to avoid multi-lighting errors

--- Main.elm
+++ Main.elm
@@ -11,36 +11,52 @@ import Music
 -- MODEL
 
 type alias Notes = List String
+type alias CharCodes = List Int
+type alias KeyMap =
+  {
+    label : String,
+    noteType : String
+  }
 
-notes : Signal Notes
-notes =
-  Signal.map Music.keysAsNotes (Signal.map Set.toList Keyboard.keysDown)
+bassNoteKeys = [ 192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187 ]
+chordKeys    = [ 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220 ]
 
 -- VIEW
 
-view : Notes -> Html
-view notesPlaying =
+view : CharCodes -> Html
+view pressedKeys =
   div [ class "view" ]
   [
-    noteList notesPlaying "bass" Music.bassNotes,
-    noteList notesPlaying "chord" Music.chordRoots
+    noteList pressedKeys bassNoteKeys,
+    noteList pressedKeys chordKeys
     ]
 
-noteList : Notes -> String -> List String -> Html
-noteList notesPlaying noteType notes =
+noteList : List Int -> List Int -> Html
+noteList pressedKeys keyList =
   let
-      noteMapper = asNoteElement notesPlaying noteType
+      noteMapper = asNoteElement pressedKeys
   in
-     div [ class "container" ] (List.map noteMapper notes)
+     div [ class "container" ] (List.map noteMapper keyList)
 
-asNoteElement : Notes -> String -> String -> Html
-asNoteElement notesPlaying noteType note =
+asNoteElement : List Int -> Int -> Html
+asNoteElement pressedKeys key =
   let
-      noteIsPlaying = List.member note notesPlaying
-      playingClass = if noteIsPlaying then "playing" else ""
-      classes = String.join " " [ noteType, "key", playingClass ]
+      keyIsPressed = List.member key pressedKeys
+      pressedClass = if keyIsPressed then "pressed" else ""
+      keyMap       = keyMapping key
+      classes = String.join " " [ keyMap.noteType, "key", pressedClass ]
   in
-     span [ class classes ] [ text note ]
+     span [ class classes ] [ text keyMap.label ]
+
+-- SIGNALS
+
+keysDown : Signal CharCodes
+keysDown =
+  Signal.map Set.toList Keyboard.keysDown
+
+notes : Signal Notes
+notes =
+  Signal.map Music.keysAsNotes keysDown
 
 -- PORTS
 
@@ -56,4 +72,37 @@ port console =
 
 main : Signal Html
 main =
-  Signal.map view notes
+  Signal.map view keysDown
+
+-- MAPPINGS
+
+keyMapping : Int -> KeyMap
+keyMapping key =
+  case key of
+    192 -> KeyMap "C3"  "bass"
+    49  -> KeyMap "C#3" "bass"
+    50  -> KeyMap "D3"  "bass"
+    51  -> KeyMap "D#3" "bass"
+    52  -> KeyMap "E3"  "bass"
+    53  -> KeyMap "F3"  "bass"
+    54  -> KeyMap "F#3" "bass"
+    55  -> KeyMap "G3"  "bass"
+    56  -> KeyMap "G#3" "bass"
+    57  -> KeyMap "A3"  "bass"
+    48  -> KeyMap "A#3" "bass"
+    189 -> KeyMap "B3"  "bass"
+    187 -> KeyMap "C4"  "bass"
+    81  -> KeyMap "C4"  "chord"
+    87  -> KeyMap "C#4" "chord"
+    69  -> KeyMap "D4"  "chord"
+    82  -> KeyMap "D#4" "chord"
+    84  -> KeyMap "E4"  "chord"
+    89  -> KeyMap "F4"  "chord"
+    85  -> KeyMap "F#4" "chord"
+    73  -> KeyMap "G4"  "chord"
+    79  -> KeyMap "G#4" "chord"
+    80  -> KeyMap "A4"  "chord"
+    219 -> KeyMap "A#4" "chord"
+    221 -> KeyMap "B4"  "chord"
+    220 -> KeyMap "C5"  "chord"
+    _   -> KeyMap ""    ""
